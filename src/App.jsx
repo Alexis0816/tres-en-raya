92621c760747cfc8dad907511dc0eb88d76f42be
Alexis Raza <gilver.raza@utec.edu.pe>
2024-05-28 16:29:42 -0500

    All the necessary

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { UpdateBoard } from './components/UpdateBoard'
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    // si ya tiene algo, no se actualiza esta posición
+    // o ya hay ganador
+    if (board[index] || winner) return
+
+    // actualizar el tablero
+    const newBoard = [...board]
+    // no se modifican los estados, se crean nuevos
+    newBoard[index] = turn
+    setBoard(newBoard) // asíncrona
+
+    // cambiar turno
+    setTurn(turn === TURNS.X ? TURNS.O : TURNS.X)
+
+    // revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner) // actualiza el estado (SI ESTO FUESE SÍNCRONO QUE NO LO ES)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>TRES EN RAYA</h1>
+      <button onClick={resetGame}>Reset del Juego</button>
+      
+      <UpdateBoard board={board} updateBoard={updateBoard}/>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    
+      <WinnerModal resetGame={resetGame} winner = {winner}/>
+
+    </main>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://alexis0816.github.io/tres-en-raya'
+})
